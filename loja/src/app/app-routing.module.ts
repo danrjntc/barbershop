689184239589575896f27a637c6ff9084b43e6d3
danrjntc <danilodamaseno16@gmail.com>
2022-03-23 19:23:59 -0300

    Iniciando

---
+++ loja/src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'pages/home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'pages/home',
+    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
+  }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
